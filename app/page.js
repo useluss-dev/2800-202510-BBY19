334273172f583c9ad7131306858e4c7082736da4
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-06 21:23:48 -0700

    Modified the dropdown category component, added styling, modified the header component and styling and added desktop styling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import ShippingInfo from "./components/ShippingInfo";
 import ListingGroup from "./components/ListingGroup";
+import Header from "./components/Header";
 
 export default function Home() {
-	return (
-		<div>
-		</div>
-	);
+  return <div></div>;
 }
