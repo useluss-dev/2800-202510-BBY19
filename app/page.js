37ccb11bd74e75ce3afb905e77f2a3dc72550c25
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-08 03:35:48 -0700

    installed formatter, added images to public, created hero component and styled it for all viewports, added tailwind variables to globals, and imported header and hero to the home page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,15 @@ import Image from 'next/image';
 import ShippingInfo from './components/ShippingInfo';
 import ListingGroup from './components/ListingGroup';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 export default function Home() {
-    return <div></div>;
+    return (
+        <div>
+            <Header />
+            <main className="mx-auto my-8 max-w-7xl px-4 sm:px-6 lg:px-8">
+                <Hero />
+            </main>
+        </div>
+    );
 }
