22f1769db745d64c5708280a49de0ea296a90734
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-08 12:41:15 -0700

    installed mongodb, created the connection in the lib folder, and created an api route for the listing

---
+++ app/api/listings/route.js
@@ -0,0 +1,23 @@
+import clientPromise from '../../lib/mongodb';
+
+export async function GET() {
+    try {
+        // await for client to connect
+        const client = await clientPromise;
+        // select the database "recompute"
+        const db = client.db('recompute');
+        // select the conllection "listings", find all documents and convert to array
+        const listings = await db.collection('listings').find({}).toArray();
+
+        //sends the resoponse back as json with a 200 code
+        return new Response(JSON.stringify(listings), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+        // catch method for any errors and send back a 500 code
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Failed to fetch listings' }), {
+            status: 500,
+        });
+    }
+}
