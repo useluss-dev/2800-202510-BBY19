8dd34ce786e5fade34010f1ec41865e262f41b6d
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-09 01:22:31 -0700

    added header to cart page and styles, and modified the cartItem styles across all screen sizes

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -6,7 +6,27 @@ function cart() {
     return (
         <div>
             <Header />
-            <CartItem />
+
+            <div className="p-6">
+                <p className="pb-4 text-sm text-gray-400">placeholder</p>
+                <h1 className="mb-4 text-2xl font-bold">Shopping cart</h1>
+
+                {/* Header Row */}
+                <div className="mx-6 flex justify-around gap-4 border-b border-gray-500 py-8 pb-2 font-semibold text-white lg:w-2/3">
+                    <div className="w-1/2 pl-6">
+                        <p className="">Product</p>
+                    </div>
+
+                    <div className="ml-5 flex w-1/2 items-center justify-evenly gap-4 md:mr-2 md:ml-0 lg:mr-8">
+                        <p className="text-center">Quantity</p>
+                        <p className="text-center">Total</p>
+                    </div>
+                </div>
+
+                {/* Cart Item */}
+                <CartItem />
+                <CartItem />
+            </div>
         </div>
     );
 }
--- app/components/CartItem.jsx
+++ app/components/CartItem.jsx
@@ -5,26 +5,27 @@ import { MdOutlineClose } from 'react-icons/md';
 
 function CartItem() {
     return (
-        <div className="flex items-center justify-around gap-4 mx-6 pb-2 border-b border-gray-500 lg:w-2/3">
-            <div>
-                <Image src="/headphones.png" alt="Product Image" width={100} height={100} />
-            </div>
+        <div className="mx-6 flex items-center justify-around gap-4 border-b border-gray-500 py-6 lg:w-2/3">
+            <div className="flex w-1/3 items-center gap-4">
+                <div>
+                    <Image src="/headphones.png" alt="Product Image" width={100} height={100} />
+                </div>
 
-            {/* Placeholder for product details */}
-            <div>
-                <h3 className="text-lg font-bold pb-1">placeholder</h3>
-                <p className="text-sm">
-                    <span className="text-gray-400">Price: </span>
-                    White
-                </p>
-                <p className="text-sm">
-                    <span className="text-gray-400">Brand: </span>
-                    Apple
-                </p>
+                {/* Placeholder for product details */}
+                <div>
+                    <h3 className="pb-1 text-lg font-bold">placeholder</h3>
+                    <p className="text-sm">
+                        <span className="text-gray-400">Price: </span>
+                        White
+                    </p>
+                    <p className="text-sm">
+                        <span className="text-gray-400">Brand: </span>
+                        Apple
+                    </p>
+                </div>
             </div>
-
             {/* Placeholder for quantity and remove button */}
-            <div className="flex items-center gap-6">
+            <div className="flex w-1/3 items-center justify-around gap-7">
                 <div className="flex items-center gap-3 rounded-xl border border-gray-300 px-4 py-2">
                     <button>
                         <GoDash />
