4b79acf3e12f47f02293e5fe33348d22b5db2e03
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-08 23:19:41 -0700

    created a cartItem component to be used in the cart page and looped through if multiple items are added

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import CartItem from '../components/CartItem';
 
 function cart() {
-    return <div>
-        <Header />
-    </div>;
+    return (
+        <div>
+            <Header />
+            <CartItem />
+        </div>
+    );
 }
 
 export default cart;
