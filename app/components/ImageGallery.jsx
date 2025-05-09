ecbd6f2dfb7ab693ebd89df4f5da08476a2967ea
Ryan Fiset <ryfiset@gmail.com>
2025-05-09 04:16:36 -0700

    Create image gallery component and display it on listing page

---
+++ app/components/ImageGallery.jsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+const images = ['/gpu.png', '/headphones.png', '/rasp.png'];
+
+function ImageGallery() {
+    const [selectedImage, setSelectedImage] = useState(images[0]);
+
+    return (
+        <div className="flex items-start space-x-4 p-4">
+            <div className="flex w-20 flex-col space-y-2">
+                {images.map((img, idx) => (
+                    <Image
+                        key={idx}
+                        src={img}
+                        width={64}
+                        height={64}
+                        onClick={() => setSelectedImage(img)}
+                        className={`cursor-pointer rounded-md transition-all ${selectedImage === img ? 'border-2 border-white' : ''}`}
+                        alt={`Thumbnail ${idx + 1}`}
+                    />
+                ))}
+            </div>
+
+            <div className="relative h-[500px] w-full rounded-md">
+                <Image src={selectedImage} fill className="rounded-md object-contain" alt="Main" />
+            </div>
+        </div>
+    );
+}
+
+export default ImageGallery;
