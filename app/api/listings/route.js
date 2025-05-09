5826429091930d0baf4184993cb81baba56e62f3
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-08 21:18:04 -0700

    added db name to env file

--- app/api/listings/route.js
+++ app/api/listings/route.js
@@ -5,7 +5,7 @@ export async function GET() {
         // await for client to connect
         const client = await clientPromise;
         // select the database "recompute"
-        const db = client.db('recompute');
+        const db = client.db(process.env.MONGODB_NAME);
         // select the conllection "listings", find all documents and convert to array
         const listings = await db.collection(process.env.MONGODB_COLLECTIONL).find({}).toArray();
 
