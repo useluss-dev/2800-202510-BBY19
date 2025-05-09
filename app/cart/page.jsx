6e616a0b682c6d16e0ba1a2e0539d35b1680a78a
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-09 02:12:49 -0700

    created a order summary component and imported it in the cart page and modified desktop styling

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import CartItem from '../components/CartItem';
+import OrderSummary from '../components/OrderSummary';
 
 function cart() {
     return (
@@ -12,20 +13,28 @@ function cart() {
                 <h1 className="mb-4 text-2xl font-bold">Shopping cart</h1>
 
                 {/* Header Row */}
-                <div className="mx-6 flex justify-around gap-4 border-b border-gray-500 py-8 pb-2 font-semibold text-white lg:w-2/3">
-                    <div className="w-1/2 pl-6">
-                        <p className="">Product</p>
+                <div className="flex flex-col justify-between px-6 lg:flex-row">
+                    <div className="lg:w-2/3">
+                        <div className="flex justify-around gap-4 border-b border-gray-500 py-8 pb-2 font-semibold text-white">
+                            <div className="w-1/2 pl-6">
+                                <p>Product</p>
+                            </div>
+                            <div className="ml-5 flex w-1/2 items-center justify-evenly gap-4 md:mr-2 md:ml-0 lg:mr-8">
+                                <p className="text-center">Quantity</p>
+                                <p className="text-center">Total</p>
+                            </div>
+                        </div>
+
+                        {/* Cart Items */}
+                        <CartItem />
+                        <CartItem />
                     </div>
 
-                    <div className="ml-5 flex w-1/2 items-center justify-evenly gap-4 md:mr-2 md:ml-0 lg:mr-8">
-                        <p className="text-center">Quantity</p>
-                        <p className="text-center">Total</p>
+                    {/* Order Summary */}
+                    <div className="mt-8 lg:mt-0 lg:ml-0 lg:flex lg:w-1/3 lg:justify-end">
+                        <OrderSummary />
                     </div>
                 </div>
-
-                {/* Cart Item */}
-                <CartItem />
-                <CartItem />
             </div>
         </div>
     );
