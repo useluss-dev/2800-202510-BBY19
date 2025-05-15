5e0065db76c0258469b1b73a865b7ee396de66ae
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-15 04:29:46 -0700

    added a conditional render if there are no items in cart and specified the height for the cart page to be screen height - header height and footer height

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -33,42 +33,46 @@ function cart() {
     const itemsInCart = cartItems.length;
 
     return (
-        <div>
+        <div className="min-h-[calc(100vh-92px-263px)] lg:min-h-[calc(100vh-140px-263px)]">
             <div className="p-6">
                 <h1 className="mb-4 text-2xl font-bold">Shopping cart</h1>
 
-                {/* Header Row */}
-                <div className="flex flex-col justify-between px-6 lg:flex-row">
-                    <div className="lg:w-2/3">
-                        <div className="grid grid-cols-[3fr_2fr_1fr_.5fr] items-center gap-4 border-b border-gray-500 py-4 font-semibold text-white">
-                            <p>Product</p>
-                            <p className="text-center">Quantity</p>
-                            <p className="text-center">Total</p>
-                            <button
-                                onClick={clearCart}
-                                className="text-right text-[#F55266] hover:cursor-pointer"
-                            >
-                                Clear
-                            </button>
+                {itemsInCart === 0 ? (
+                    <p>Your cart is empty</p>
+                ) : (
+                    <div className="flex flex-col justify-between px-6 lg:flex-row">
+                        {/* Header Row */}
+                        <div className="lg:w-2/3">
+                            <div className="grid grid-cols-[3fr_2fr_1fr_.5fr] items-center gap-4 border-b border-gray-500 py-4 font-semibold text-white">
+                                <p>Product</p>
+                                <p className="text-center">Quantity</p>
+                                <p className="text-center">Total</p>
+                                <button
+                                    onClick={clearCart}
+                                    className="text-right text-[#F55266] hover:cursor-pointer"
+                                >
+                                    Clear
+                                </button>
+                            </div>
+
+                            {/* Cart Items */}
+                            {cartItems.map((item) => (
+                                <CartItem
+                                    key={item.id}
+                                    item={item}
+                                    quanAdd={quanIncrease}
+                                    quanMinus={quanDecrease}
+                                    remove={removeItem}
+                                />
+                            ))}
                         </div>
 
-                        {/* Cart Items */}
-                        {cartItems.map((item) => (
-                            <CartItem
-                                key={item.id}
-                                item={item}
-                                quanAdd={quanIncrease}
-                                quanMinus={quanDecrease}
-                                remove={removeItem}
-                            />
-                        ))}
+                        {/* Order Summary */}
+                        <div className="mt-8 lg:mt-0 lg:ml-0 lg:flex lg:w-1/3 lg:justify-end">
+                            <OrderSummary cartTotalPrice={totalPrice} cartLength={itemsInCart} />
+                        </div>
                     </div>
-
-                    {/* Order Summary */}
-                    <div className="mt-8 lg:mt-0 lg:ml-0 lg:flex lg:w-1/3 lg:justify-end">
-                        <OrderSummary cartTotalPrice={totalPrice} cartLength={itemsInCart} />
-                    </div>
-                </div>
+                )}
             </div>
         </div>
     );
