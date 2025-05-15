869dc9e527e47f28a50fc0ee4d9ba9f907f5ac0e
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-05-15 03:39:12 -0700

    modified the cart header and cartItem styles to used grid and align the header with the cartItem component details, also added hover effects, changed remove button color and added a clear button

--- app/components/CartItem.jsx
+++ app/components/CartItem.jsx
@@ -6,38 +6,39 @@ import { MdOutlineClose } from 'react-icons/md';
 function CartItem({ item, quanAdd, quanMinus, remove }) {
     const itemTotalPrice = (item.price * item.quantity).toFixed(2);
     return (
-        <div className="mx-6 flex items-center justify-around gap-4 border-t border-gray-500 py-6 lg:mx-0 lg:w-full">
-            <div className="flex w-1/3 items-center gap-4">
+        <div className="grid grid-cols-[2fr_1fr_1fr_auto] items-center justify-center gap-4 border-t border-gray-500 py-4 lg:mx-0 lg:w-full">
+            <div className="flex min-w-0 items-center gap-4">
+                <Image src={item.image} alt={item.name} width={100} height={100} />
                 <div>
-                    <Image src={item.image} alt={item.name} width={100} height={100} />
-                </div>
-
-                <div>
-                    <h3 className="pb-1 text-lg font-bold">{item.name}</h3>
+                    <h3 className="pb-1 font-bold">{item.name}</h3>
                     <p className="text-sm">
-                        <span className="text-gray-400">Price: </span>
-                        {item.price}
+                        <span className="text-gray-400">Price: </span> ${item.price}
                     </p>
                     <p className="text-sm">
-                        <span className="text-gray-400">Brand: </span>
-                        {item.brand}
+                        <span className="text-gray-400">Brand: </span> {item.brand}
                     </p>
                 </div>
             </div>
 
-            <div className="flex w-1/3 items-center justify-around gap-7">
-                <div className="flex items-center gap-3 rounded-xl border border-gray-300 px-4 py-2">
-                    <button onClick={() => quanMinus(item.id)}>
+            <div className="flex justify-center">
+                <div className="flex items-center justify-center gap-3 rounded-xl border border-gray-300 px-4 py-2 align-middle md:w-2/3 lg:w-1/2">
+                    <button onClick={() => quanMinus(item.id)} className='hover:cursor-pointer'>
                         <GoDash />
                     </button>
                     <span>{item.quantity}</span>
-                    <button onClick={() => quanAdd(item)}>
+                    <button onClick={() => quanAdd(item)} className='hover:cursor-pointer'>
                         <GoPlus />
                     </button>
                 </div>
+            </div>
 
-                <p>${itemTotalPrice}</p>
-                <button onClick={() => remove(item.id)}>
+            <p className="text-center font-semibold">${itemTotalPrice}</p>
+
+            <div>
+                <button
+                    onClick={() => remove(item.id)}
+                    className="text-xl text-[#F55266] hover:cursor-pointer"
+                >
                     <MdOutlineClose />
                 </button>
             </div>
