e355f69a86a0e63999bf5d0043f38cba6e65159f
Ryan Fiset <ryfiset@gmail.com>
2025-05-21 07:06:19 -0700

    Fix lint errors and format code

--- app/components/ReviewCreate.jsx
+++ app/components/ReviewCreate.jsx
@@ -1,34 +1,37 @@
 'use client';
-import React, { useState, useEffect, useRef } from 'react';
-
-
+import React from 'react';
 
 export default function ReviewCreate() {
- 
-
     return (
-        <div className="w-full rounded-2xl bg-[#1B273A] py-10 shadow-lg flex flex-col space-y-4 px-6">
-  <div>Item Name</div>
+        <div className="flex w-full flex-col space-y-4 rounded-2xl bg-[#1B273A] px-6 py-10 shadow-lg">
+            <div>Item Name</div>
 
-  <div className="flex items-center space-x-2">
-    <div>Rating:</div>
-    <select className=" appearance-none hover: rounded px-2 py-1 bg-[#556780] fill-current text-white border border-transparent focus:border-[#F55266] focus:border-2 outline-none">
-      <option>Poor</option>
-      <option>Ok</option>
-      <option>Good</option>
-    </select>
-  </div>
+            <div className="flex items-center space-x-2">
+                <div>Rating:</div>
+                <select className="hover: appearance-none rounded border border-transparent bg-[#556780] fill-current px-2 py-1 text-white outline-none focus:border-2 focus:border-[#F55266]">
+                    <option>Poor</option>
+                    <option>Ok</option>
+                    <option>Good</option>
+                </select>
+            </div>
 
-  <div>Input your review title here:</div>
-  <textarea type="text" className="bg-[#556780] px-2 py-1 rounded text-white border border-transparent focus:border-[#F55266] focus:border-2 outline-none lg:w-1/2" id="reviewTitle" />
+            <div>Input your review title here:</div>
+            <textarea
+                type="text"
+                className="rounded border border-transparent bg-[#556780] px-2 py-1 text-white outline-none focus:border-2 focus:border-[#F55266] lg:w-1/2"
+                id="reviewTitle"
+            />
 
-  <div>Input your review description here:</div>
-  <textarea type="text" className="bg-[#556780] px-2 py-1 rounded text-white border border-transparent focus:border-[#F55266] focus:border-2 outline-none lg:w-1/2" id="reviewBody" />
+            <div>Input your review description here:</div>
+            <textarea
+                type="text"
+                className="rounded border border-transparent bg-[#556780] px-2 py-1 text-white outline-none focus:border-2 focus:border-[#F55266] lg:w-1/2"
+                id="reviewBody"
+            />
 
-  <button className="mt-4 bg-[#F55266] text-white px-4 py-2 rounded hover:bg-[#f55265c8] md:w-1/5 lg:w-1/7">
-    Submit
-  </button>
-</div>
-
+            <button className="mt-4 rounded bg-[#F55266] px-4 py-2 text-white hover:bg-[#f55265c8] md:w-1/5 lg:w-1/7">
+                Submit
+            </button>
+        </div>
     );
 }
