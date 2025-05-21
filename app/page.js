a0c2721d8afa4245b9ba6246cc2a10037519794d
jprice916 <91910012+jprice916@users.noreply.github.com>
2025-05-20 21:53:52 -0700

    fixed the page.js

--- app/page.js
+++ app/page.js
@@ -2,15 +2,13 @@ import React from 'react';
 // import Image from 'next/image';
 // import ShippingInfo from './components/ShippingInfo';
 // import ListingGroup from './components/ListingGroup';
-import Review from './components/ReviewCreate'
 import Hero from './components/Hero';
 
 export default function Home() {
     return (
         <div>
             <main className="mx-auto my-8 max-w-7xl px-4 sm:px-6 lg:px-8">
-                {/* <Hero /> */}
-                <Review />
+                <Hero />
             </main>
         </div>
     );
